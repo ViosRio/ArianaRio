ca630e9689b55400b7202b3128af1e57aab6367e
VİOS HOLDİNG LTD. <erollitex@gmail.com>
2025-04-12 03:58:33 +0300

    Update subs.py

--- subs.py
+++ subs.py
@@ -1,55 +1,41 @@
 import json
 import os
-from subs import abone_ekle, abone_sil, abonelik_listesi
 
-SUBS_FILE = "data/subs.json"
+DATA_FILE = "data/subs.json"
 
-# Dosya var mı kontrolü
-if not os.path.exists("data"):
-    os.makedirs("data")
-if not os.path.exists(SUBS_FILE):
-    with open(SUBS_FILE, "w") as f:
-        json.dump({}, f)
+def load_data():
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE, "r") as f:
+            return json.load(f)
+    return {}
 
-# JSON oku
-def load_subs():
-    with open(SUBS_FILE, "r") as f:
-        return json.load(f)
+def save_data(data):
+    with open(DATA_FILE, "w") as f:
+        json.dump(data, f)
 
-# JSON kaydet
-def save_subs(data):
-    with open(SUBS_FILE, "w") as f:
-        json.dump(data, f, indent=4)
-
-# Abonelik ekle
 def abone_ekle(chat_id, username):
-    data = load_subs()
-    str_id = str(chat_id)
-    if str_id not in data:
-        data[str_id] = []
-    if username not in data[str_id]:
-        data[str_id].append(username)
-        save_subs(data)
+    data = load_data()
+    chat_id = str(chat_id)
+    if chat_id not in data:
+        data[chat_id] = []
+    if username not in data[chat_id]:
+        data[chat_id].append(username)
+        save_data(data)
         return True
     return False
 
-# Abonelik sil
 def abone_sil(chat_id, username):
-    data = load_subs()
-    str_id = str(chat_id)
-    if str_id in data and username in data[str_id]:
-        data[str_id].remove(username)
-        if not data[str_id]:
-            del data[str_id]
-        save_subs(data)
+    data = load_data()
+    chat_id = str(chat_id)
+    if chat_id in data and username in data[chat_id]:
+        data[chat_id].remove(username)
+        save_data(data)
         return True
     return False
 
-# Belirli kullanıcının abonelik listesi
 def abonelik_listesi(chat_id):
-    data = load_subs()
+    data = load_data()
     return data.get(str(chat_id), [])
 
-# Tüm abonelikler (checker için)
-def tum_abonelikler():
-    return load_subs()
+def get_all_subscriptions():
+    return load_data()
